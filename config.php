2b93e4fb770179431798fd0c899d9a3004cba36a
Mamadou000 <145232773+Mamadou000@users.noreply.github.com>
2024-10-18 10:38:01 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -4,12 +4,17 @@ session_start();
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_PORT', '8080');
+define('DB_PORT', '3306');
 
 
 define('DB_NAME', 'secu');
 define('DB_USER', 'root');
-define('DB_PASS', 'azerty');
+define('DB_PASS', 'root');
 
 // connect to database
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+define('ROOT_PATH', realpath(dirname(__FILE__)));
