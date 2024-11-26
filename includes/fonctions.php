fa9d7ad1c5caf24db1cf55a1cbb7481b0cf0173e
Mamadou000 <145232773+Mamadou000@users.noreply.github.com>
2024-11-26 21:26:38 +0100

    Add files via upload

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -10,5 +10,22 @@ function getnews()
     return $result;
 }
 
+function getUsers() 
+{
+    global $conn;
 
+    $sql = "SELECT * FROM utilisateur";
+    $result = mysqli_query($conn, $sql);
+
+    return $result;
+}
+
+function deleteUser($id) 
+{
+    global $conn;
+
+    $sql = "DELETE FROM utilisateur WHERE id = $id";
+
+    return mysqli_query($conn, $sql);
+}
 
